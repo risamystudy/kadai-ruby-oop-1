7102bb2926f0fcb3cca1278a28ae06c18b759976
risa <risa.mystudy@gmail.com>
2021-09-27 10:11:32 +0000

    anothertry

--- team.rb
+++ team.rb
@@ -1,41 +1,38 @@
 class Team
-  
-   attr_accessor :name, :win, :lose, :draw
-   
-    #（チーム6つ分入れるので引数もたせる）名前、勝ち数、負け数、引き分け数
-   def initialize(name, win, lose, draw) 
-       self.name = name
-       self.win = win
-       self.lose = lose
-       self.draw = draw
+    #変数定義
+    attr_accessor :name, :win, :lose, :draw
+    
+    #初期化
+    def initialize(name, win, lose, draw)
+        self.name = name
+        self.win = win
+        self.lose = lose
+        self.draw = draw
     end
     
-    #勝率計算式
+    #勝率の計算メソッド
     def calc_win_rate
         self.win.to_f / (self.win + self.lose)
     end
     
-    #表示させるメソッド
+    #表示用メソッド
     def show_team_result
-        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw} 分、勝率は #{self.calc_win_rate}です。"
     end
-    
-    
-    
 end
 
-#インスタンス生成
-team1 = Team.new("Giants", 67, 45, 8)
-team2 = Team.new("Tigers", 60, 53, 7)
-team3 = Team.new("Dragons", 60, 55, 5)
-team4 = Team.new("BayStars", 56, 58, 6)
-team5 = Team.new("Carp", 52, 56, 12)
-team6 = Team.new("Swallows", 41, 69, 10)
+#6チーム分インスタンス生成
+team_1 = Team.new('Giants', 67, 45, 8)
+team_2 = Team.new('Tigers', 60, 53, 7)
+team_3 = Team.new('Dragons', 60, 55, 5)
+team_4 = Team.new('BayStars', 56, 58, 6)
+team_5 = Team.new('Carp', 52, 56, 12)
+team_6 = Team.new('Swallows', 41, 69, 10)
 
-#出力   //ここをもっとまとめて書きたい…
-team1.show_team_result
-team2.show_team_result
-team3.show_team_result
-team4.show_team_result
-team5.show_team_result
-team6.show_team_result
+#show_team_resultを呼び出して表示する
+team_1.show_team_result
+team_2.show_team_result
+team_3.show_team_result
+team_4.show_team_result
+team_5.show_team_result
+team_6.show_team_result
